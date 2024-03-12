2004d0e7be2452ffc8c9e657cae2453dd37c4cd6
cinpean <sebycinpean@gmail.com>
2024-03-12 16:02:01 +0200

    Added intersection point to prediction path

--- Source_code/Raspberry_pi/picamera2_for_ros2_pk2/picamera2_for_ros2_pkg/cameratopic_node_v4.py
+++ Source_code/Raspberry_pi/picamera2_for_ros2_pk2/picamera2_for_ros2_pkg/cameratopic_node_v4.py
@@ -36,7 +36,7 @@ def extend_line(x1, y1, x2, y2, length):
 def slider_for_debug():
     cv2.namedWindow('Manual_motor',cv2.WINDOW_NORMAL)
     cv2.createTrackbar('thrs1', 'Manual_motor', 100, 200, ImagePublisher.callback)   
-    cv2.createTrackbar('thrs2', 'Manual_motor', 100, 200, ImagePublisher.callback_4)   
+    cv2.createTrackbar('thrs2', 'Manual_motor', 100, 200, ImagePublisher.callback_2)   
     ch = None
     while ch != 27:
         ch = cv2.waitKey(0)
@@ -107,6 +107,13 @@ class ImagePublisher(Node):
         result_string = f"B{value}"
         Send_msg_motor().publish_message(result_string)
 
+    def callback_2(x):
+        ImagePublisher.y_from_slider = x
+        value = (int(ImagePublisher.interpolate_stepper_steps_2( ImagePublisher.from_px_to_mm_16(ImagePublisher.y_from_slider))))
+        print (value)
+        result_string = f"B{value}"
+        Send_msg_motor().publish_message(result_string)
+
     def __init__(self):
         super().__init__("image_publisher")
         self.pub = self.create_publisher(Image, "/image", 10)
@@ -121,10 +128,10 @@ class ImagePublisher(Node):
         video_config["controls"]["ExposureTime"] = 40000 # max exposure time is ~37000
         self.cap.configure(video_config)
         self.cap.start()
-        x1, y1 = 23, 7
+        x1, y1 = 30, 7  #23,7
         x2, y2 = 420, 7
         x3, y3 = 420, 238
-        x4, y4 = 23, 238
+        x4, y4 = 30, 238 # 23,238
         self.kf = KalmanFilter()
         
         if not ImagePublisher.lovitura:
@@ -133,6 +140,7 @@ class ImagePublisher(Node):
         # Create ROI vertices array
         self.roi_vertices = np.array([[x1, y1], [x2, y2], [x3, y3], [x4, y4]], np.int32)
 
+    # used for mapping read values in regards to stepper driver microstepping _16 stands for 16/1 microstepping
     def interpolate_stepper_steps_16(distance):
         table = [(0, 0), (18, 16), (50, 32), (68, 48), (85, 64), (98, 80), (111, 96), (123, 112), (134, 128), (143, 144), (152, 160), (160, 176), (168, 192)]
         for i in range(len(table) - 1):
@@ -155,7 +163,8 @@ class ImagePublisher(Node):
         if(d2-d1 == 0):
             return ImagePublisher.interpolated_steps 
         return ImagePublisher.interpolated_steps
-    
+
+    # used for mapping read values in regards to stepper driver microstepping _4 stands for 4/1 microstepping
     def interpolate_stepper_steps_4(distance):
         table = [(0, 0), (18, 40), (54, 80), (74, 120), (90, 160), (104, 200), (116, 240), (128, 280), (139, 320), (148, 360), (157, 400), (165, 440), (170, 450)]
         for i in range(len(table) - 1):
@@ -179,12 +188,32 @@ class ImagePublisher(Node):
             return ImagePublisher.interpolated_steps 
         return ImagePublisher.interpolated_steps
 
+    # used for mapping read values in regards to stepper driver microstepping _2 stands for 2/1 microstepping
+    def interpolate_stepper_steps_2(distance):
+        table = [(0, 0), (20, 20), (50, 40), (67, 60), (84, 80), (97, 100), (111, 120), (124, 140), (133, 160), (143, 180), (152, 200), (160, 220), (170, 240)]
+        for i in range(len(table) - 1):
+            if table[i][0] <= distance <= table[i + 1][0]:
+                d1, s1 = table[i]
+                d2, s2 = table[i + 1]
+                break
+            elif distance > table[len(table) - 1][0]:
+                d1, s1 = table[len(table) - 2]
+                d2, s2 = table[len(table) - 1]
+                break
+            else :
+                d1, s1 = table[0]
+                d2, s2 = table[0]                  
+
+        # Apply linear interpolation
+        if(d2-d1 != 0):
+            ImagePublisher.interpolated_steps = s1 + ((distance - d1) * (s2 - s1) / (d2 - d1))
+            
+        if(d2-d1 == 0):
+            return ImagePublisher.interpolated_steps 
+        return ImagePublisher.interpolated_steps
+
     def from_px_to_mm_16(px_val):
-        y = ((px_val-90)*168)/69
-        return y
-    
-    def from_px_to_mm_4(px_val):
-        y = ((px_val-90)*480)/69
+        y = ((px_val-90)*159)/69
         return y
 
     def timer_callback(self):
@@ -198,7 +227,7 @@ class ImagePublisher(Node):
         img_hsv = cv2.cvtColor(masked_frame,cv2.COLOR_BGR2HSV)
         img_hsv = self.increase_brightness(img_hsv,value=80) #trying to imporve brightness
         
-        lower_blue= np.array([164,88,75]) #164,88,75  #155,140,70                                       #old vals 165,118,100
+        lower_blue= np.array([164,140,65]) #164,88,75  #155,140,70  #155,81,49 for high light enviromnet                                  #old vals 165,118,100
         upper_blue= np.array([179,255,255]) #179,50,255 commented ones, for white and shades of white   #old vals 188,255,255
 
         mask_blue= cv2.inRange(img_hsv, lower_blue, upper_blue)
@@ -238,10 +267,11 @@ class ImagePublisher(Node):
             cv2.line(img, (int(predicted[0]),int(predicted[1])), (x3, y3), (250, 250, 0), 2)
 
             
-            for x_line in range(x,440) :
+            for x_line in range(x,430) :
                 y = center[1]
-                # Y values from 75 & 165 are the extermities from the GoalKeeper penlaty area 
-                if y > 75 and y < 165 and x_line == 404 and center > (344,y) and center<(420,y) and ImagePublisher.lovitura == 0:
+                # Y values from 90 & 165 are the extermities from the GoalKeeper penlaty area 
+                # used for moving the servomotor in order to hit the ball
+                if y > 90 and y < 165 and x_line == 404 and center > (344,y) and center<(420,y) and ImagePublisher.lovitura == 0:
                     self.get_logger().info('inside : "%d"' % y)
                     Send_msg_motor().publish_message('S60')
                     ImagePublisher.lovitura = 1
@@ -250,32 +280,44 @@ class ImagePublisher(Node):
                     ImagePublisher.lovitura = 0
                     self.get_logger().info('inside : "%d"' % center[0])
                     Send_msg_motor().publish_message('S0')
-                
 
                 # move around following the ball anywhere in the field, position greater than the half of the field
-                if y > 83 and y < 165 and x_line == 404 and center > (170,y) and center < (410,y):
-                    if x3 >= 400 and y3 > 83 and y3 < 165 :
-                        self.get_logger().info('px_ball_prediction : "%d"' % y3)
-                        self.get_logger().info('px_to_mm_prediction : "%d"' % ImagePublisher.from_px_to_mm_16(y3))
-                        value = (int(ImagePublisher.interpolate_stepper_steps_4( ImagePublisher.from_px_to_mm_16(y3))))
-                        self.get_logger().info('Linear_prediction : "%d"' % value) 
-                        result_string = f"B{value}"
-                        Send_msg_motor().publish_message(result_string)
+                # x,y coordonates of the center of the ball 
+                if y > 83 and y < 160 and x_line == 404 and center > (170,y) and center < (410,y):
+                    
+                    # script used for determining the intersection of the prediction line with the GK line
+                    if x3 != center[0] and x3 > 390 and y3 > 89 and y3 < 160 :
+                        intersection_point_y = int((((y3-center[1])/(x3-center[0]))*(404-center[0]))+center[1])
+                        if intersection_point_y > 0: 
+                            self.get_logger().info('Intersected w 404 : "%d"' % intersection_point_y)
+                            value = (int(ImagePublisher.interpolate_stepper_steps_2( ImagePublisher.from_px_to_mm_16(y3))))
+                            result_string = f"B{value}"
+                            Send_msg_motor().publish_message(result_string)
+
+                    # if x3 >= 400 and x3<425 and y3 > 89 and y3 < 160 :
+                    #     self.get_logger().info('px_ball_prediction : "%d"' % y3)
+                    #     self.get_logger().info('px_to_mm_prediction : "%d"' % ImagePublisher.from_px_to_mm_16(y3))
+                    #     value = (int(ImagePublisher.interpolate_stepper_steps_2( ImagePublisher.from_px_to_mm_16(y3))))
+                    #     self.get_logger().info('Linear_prediction : "%d"' % value)
+                        
+                    #     result_string = f"B{value}"
+                    #     Send_msg_motor().publish_message(result_string)
                     else :
                         self.get_logger().info('px_ball : "%d"' % y)
                         self.get_logger().info('px_to_mm : "%d"' % ImagePublisher.from_px_to_mm_16(y))
-                        value = (int(ImagePublisher.interpolate_stepper_steps_4( ImagePublisher.from_px_to_mm_16(y))))
+                        value = (int(ImagePublisher.interpolate_stepper_steps_2( ImagePublisher.from_px_to_mm_16(y))))
                         self.get_logger().info('Linear : "%d"' % value) 
                         result_string = f"B{value}"
                         Send_msg_motor().publish_message(result_string)
 
         # draws the path the ball has taken
-        for i in range(1, len(ImagePublisher.object_positions)):
-            cv2.line(img, ImagePublisher.object_positions[i - 1], ImagePublisher.object_positions[i], (0, 255, 0), 2)
+        # for i in range(1, len(ImagePublisher.object_positions)):
+            # cv2.line(img, ImagePublisher.object_positions[i - 1], ImagePublisher.object_positions[i], (0, 255, 0), 2)
 
         # draw centerline & football quads of the Goal Keeper 
         cv2.rectangle(img, (376, 85), (424, 159), (98, 0, 255), 1)
         cv2.line(img, (0, ImagePublisher.y_from_slider), (800, ImagePublisher.y_from_slider), (64, 80, 255), 1)
+
 
         # publish img to topic
         # self.pub.publish(self.bridge.cv2_to_imgmsg(img, "rgb8"))
